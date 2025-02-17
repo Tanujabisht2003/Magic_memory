7e12d8917c7de93cad8d51d8e7b05e3c9e73d19d
Tanuja Bisht <tannubisht2021@gmail.com>
2025-02-17 19:25:34 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+// use effect is used to handle side effect (start anew game, checking for matches)
+// use state used to manage game state(choices , turn, cards)
 import { useEffect,useState } from 'react'
 import './App.css';
 import SingleCard from './components/SingleCard'
 
+// this is a aray of card object with image source and matched flag
 const cardImages = [
   {"src": "img/helmet-1.png" , matched: false},
   {"src": "img/potion-1.png", matched: false },
@@ -11,30 +14,52 @@ const cardImages = [
   {"src": "img/sword-1.png" , matched: false}
 ]
 function App() {
-
+// when player clicks a card choice one is setwhen player click a second card choice two is set if both match matched become true and update in cards and if not match card will flip after 1 second
+// turns will incremet after every two card selection
+// disabled prevents extra click during the checking phase
+// cards is an state variable that stores an array of cards
+// setcards is a function to update cards
+//  useState([])-it initializes cards as an empty array
   const [cards, setCards] = useState([])
+  // turns initial value is 0
+  // set turns update when every two card is selected
+  // turns keep track of how many turns player has taken 
   const [turns, setTurns] = useState(0)
   const [choiceOne, setChoiceOne] = useState(null)
   const [choiceTwo, setChoiceTwo] = useState(null)
   const [disabled, setDisabled] = useState(false)
 
-  // shuffle cards
+  // shuffle cards function
+   // duplicate the cards ,Shuffle the cards in random order and Loop through each card, make a copy of the card object , and give it a random ID to each card .
+    // then store the modifies array in shuffled cards
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }))
 
+      // Reset the Game State when a new game is start
     setChoiceOne(null)
     setChoiceTwo(null)
     setCards(shuffledCards)
     setTurns(0)
   }
-  // handle a choice
+  // handlechoice function takes card as a parameter
+  // card represents the card the player just clicked
+  // if choiceone is already filled then card is stored in choice two and if empty then it store the card in choice one
   const handleChoice = (card) => {
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
   }
 
   // compare two selected cards
+  // it work only when player select two cards
+  // check choice one and choicetwo are selected or not if selected then set disabled will true 
+  // then check both src are matched then cards state will update 
+  // we will loop through each card which is pointed out by card in prevcards array and 
+  // then if card and choice one source are matched then it will create a new object of card with all its properties and true value assign to matched 
+  // if not then it will return card 
+  // reset the selected card after the cards are marked as matched
+  // if cards not match then wait for 1 second after that  it will call reset turn function 
+
   useEffect(() => {
     if(choiceOne && choiceTwo){
       setDisabled(true)
@@ -48,15 +73,13 @@ function App() {
             }
           })
         })
-        // console.log('those cards match')
         resetTurn()
       }else{
-        // console.log('those cards do not match')
-        // resetTurn()
         setTimeout(() => resetTurn(), 1000)
       }
     }
-  }, [choiceOne, choiceTwo])
+    // this are the dependency array which tell usereffect when to run 
+  }, [choiceOne, choiceTwo]) 
 
   console.log(cards)
 
@@ -67,13 +90,15 @@ function App() {
     setTurns(pevTurns => pevTurns + 1)
     setDisabled(false)
   }
-
+// this useeffect run only once because the dependency array is empty
+// this call shufflecards function 
   useEffect(() => {
     shuffleCards()
   }, [])
   
   return (
     <div className="App">
+    {/* title of the game */}
       <h1>Magic Match</h1>
       <button onClick={shuffleCards}>New Game</button>
 
@@ -82,6 +107,7 @@ function App() {
           <SingleCard key={card.id} card={card} handleChoice={handleChoice} flipped={card === choiceOne || card === choiceTwo || card.matched } disabled={disabled}/>
         ))}
       </div>
+      {/* display the no of turns */}
       <p>Turns: {turns}</p>
     </div>
   );
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -3,12 +3,14 @@ import './SingleCard.css'
 export default function SingleCard({ card, handleChoice, flipped, disabled }) {
 
   const handleClick = () => {
+    // if disable is true it does nothing if it false then it will call handle choice function
     if(!disabled){
       handleChoice(card)
     }
   }
     return ( 
         <div className="card">
+          {/* if flipped is true then class flipped is applied (css) and if fliiped id false it remains an empty string menaing the card remains face down */}
           <div className={flipped ? "flipped" : ""}>
             <div>
               <img className="front" src={card.src} alt="card front" />
